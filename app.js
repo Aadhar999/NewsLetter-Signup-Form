7d4b1b383b6598cb4ae768e5130aec1eda7274c0
Aadhar Gupta <aadhargupta65@gmail.com>
2022-07-01 19:21:46 +0530

    Added Procfile and port for Heroku server

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.post('/failure',function(req,res) {
 });
 
 
-app.listen(3000,function (){
+app.listen(process.env.PORT || 3000,function (){    // now app will listen on 3000 port locally as well as on specified port given by heroku server.
     console.log('Server Started at port 3000');
 });
 
